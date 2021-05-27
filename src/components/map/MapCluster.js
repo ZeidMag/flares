751adcc95a7df042e99d2ae98acd58ba7bc8eecd
Mohammed <mindxcode@gmail.com>
2021-05-27 20:39:37 +0200

    >>>

--- src/components/map/MapCluster.js
+++ src/components/map/MapCluster.js
@@ -1,29 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
   MarkerClusterer,
-} from '@react-google-maps/api';
-import ZonesList from './ZonesList';
-import { useSelector } from 'react-redux';
+} from "@react-google-maps/api";
+import ZonesList from "./ZonesList";
+import { useSelector } from "react-redux";
 
-import './Map.css';
-import './Sidebar.css';
+import "./Map.css";
+import "./Sidebar.css";
 // import GreenMarker from '../../assets/images/green-marker2.png';
 // import mapStyle from './mapStyle';
 
 const Map = () => {
   const { flareCount, roundFactor } = useSelector((state) => state.temp);
   const [maxMarker, setMaxMarker] = useState(0);
-  const resetMaxMarker = () => {
-    setMaxMarker(0);
-    console.log('resetted');
+  const resetMaxMarker = (x) => {
+    var lengths = x.clusters.map(function (cluster) {
+      return cluster.markers.length;
+    });
+    let maxlengthMath = Math.max(...lengths);
+
+    setMaxMarker(maxlengthMath);
+    console.log("resetted");
+  };
+  const setMaxMarkerOnEnd = (x) => {
+    console.log("resetted on end");
   };
 
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCSLXG5j-CqSB0zR476bJFXOSQ9sWuXqH0',
+    googleMapsApiKey: "AIzaSyCSLXG5j-CqSB0zR476bJFXOSQ9sWuXqH0",
   });
 
   const [zone, setZone] = useState(null);
@@ -47,8 +55,8 @@ const Map = () => {
     setSelectedZones(newList);
   };
 
-  if (loadError) return 'Error loading maps';
-  if (!isLoaded) return 'Loading Maps';
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
   return (
     <>
       {/* <div className="sidebar">hi</div> */}
@@ -60,14 +68,14 @@ const Map = () => {
               .filter((zone) => zone.selected === true)
               .map((item, i) => (
                 <li key={i}>
-                  Zone Coordinates: ({item.lat}, {item.lng}). it has{' '}
-                  {item.count} flare{item.count > 1 && 's'}
+                  Zone Coordinates: ({item.lat}, {item.lng}). it has{" "}
+                  {item.count} flare{item.count > 1 && "s"}
                 </li>
               ))}
           </ol>
         ) : null}
         <GoogleMap
-          mapContainerStyle={{ width: '100vw', height: '70vh' }}
+          mapContainerStyle={{ width: "100vw", height: "70vh" }}
           zoom={14}
           center={{ lat: 32.8667339, lng: 13.2017534 }}
           // options={{
@@ -79,10 +87,11 @@ const Map = () => {
           <MarkerClusterer
             zoomOnClick={false}
             onClusteringBegin={resetMaxMarker}
+            onClusteringEnd={setMaxMarkerOnEnd}
             calculator={(markers, numstyle) => {
-              if (markers.length > maxMarker) {
-                setMaxMarker(markers.length);
-              }
+              // if (markers.length > maxMarker) {
+              //   setMaxMarker(markers.length);
+              // }
               if (markers.length >= (maxMarker * 4) / 5) {
                 return { text: markers.length, index: 5 };
               }
@@ -114,27 +123,29 @@ const Map = () => {
                   position={currentZone}
                   clusterer={clusterer}
                   label={{
-                    color: '#faa',
-                    fontWeight: 'bold',
-                    fontSize: '1.5rem',
+                    color: "#faa",
+                    fontWeight: "bold",
+                    fontSize: "1.5rem",
                     text: `${currentZone.count}`,
                   }}
                   icon={
                     currentZone.selected
                       ? {
-                          path: 'M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z',
-                          fillColor: 'yellow',
+                          path:
+                            "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                          fillColor: "yellow",
                           fillOpacity: 0.9,
                           scale: 2,
-                          strokeColor: 'gold',
+                          strokeColor: "gold",
                           strokeWeight: 2,
                         }
                       : {
-                          path: 'M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z',
-                          fillColor: 'green',
+                          path:
+                            "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                          fillColor: "green",
                           fillOpacity: 0.9,
                           scale: 2,
-                          strokeColor: 'limegreen',
+                          strokeColor: "limegreen",
                           strokeWeight: 2,
                         }
                   }
@@ -145,7 +156,7 @@ const Map = () => {
                     zone.lat === currentZone.lat &&
                     zone.lng === currentZone.lng && (
                       <InfoWindow onCloseClick={clearMarker}>
-                        <div style={{ fontSize: '2rem' }}>{zone.count}</div>
+                        <div style={{ fontSize: "2rem" }}>{zone.count}</div>
                       </InfoWindow>
                     )}
                 </Marker>
