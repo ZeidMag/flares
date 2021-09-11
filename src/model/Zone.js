20f22c8af9fbde068e6e3d5a496601dcce475b94
Zeid Magboub <zeid.magboub@litc.ly>
2021-09-11 18:13:30 +0200

    Updates

--- src/model/Zone.js
+++
@@ -1,9 +0,0 @@
-class Zone {
-  constructor(latitude, longitude, roundFactor) {
-    this.latitude = Math.round(latitude * roundFactor) / roundFactor;
-    this.longitude = Math.round(longitude * roundFactor) / roundFactor;
-    this.selected = false;
-  }
-}
-
-export default Zone;
--- src/todo.md
+++
@@ -1,9 +0,0 @@
-# TODO:
-
-## complete comments
-
-## add priority / type selection to create ticket **_(Done)_**
-
-## Change createTicket method: return with only the new ticket and add it to reducer
-
-## Add who ticket-initiator to db
